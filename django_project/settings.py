830f907787d776025fe448f86b0acb59a1f9b8eb
Kazuki Maehara <treize.citron13@gmail.com>
2022-08-28 18:04:00 +0900

    forms and user accounts

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "accounts.apps.AccountsConfig",
     "blog.apps.BlogConfig",
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,3 +125,6 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath("static")), ]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
